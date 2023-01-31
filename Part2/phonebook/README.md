140b95069946303e2929767c9973c8dd109b78c9
fmossatto <fernando.a.mossatto@hotmail.com>
2023-01-31 19:19:32 -0300

    Part 2 - App phonebook - exercise 2.11

--- Part2/phonebook/README.md
+++ Part2/phonebook/README.md
@@ -1,3 +1,5 @@
 # APP phonebook
 #### Commit at 30/01/2023
 Exercises 2.6 - 2.10
+#### Commit at 31/01/2023
+Exercises 2.11
--- Part2/phonebook/src/App.js
+++ Part2/phonebook/src/App.js
@@ -1,13 +1,22 @@
-import { useState } from 'react';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import Filter from './components/Filter';
 import PersonForm from './components/PersonForm';
 import Persons from './components/Persons';
 
 const App = () => {
-    const [persons, setPersons] = useState([{ name: 'Arto Hellas', number: '040-1234567' }]);
+    const [persons, setPersons] = useState([]);
     const [newName, setNewName] = useState('');
     const [newNumber, setNewNumber] = useState('');
     const [search, setSearch] = useState('');
+
+    const getData = () => {
+        axios.get('http://localhost:3001/persons').then((response) => {
+            setPersons(response.data);
+        });
+    };
+
+    useEffect(getData, []);
 
     const handleSubmit = (event) => {
         event.preventDefault();
